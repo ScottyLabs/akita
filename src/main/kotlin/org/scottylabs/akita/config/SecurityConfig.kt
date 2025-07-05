1a3bf4e68eec7549cd651beaaa0201e492be9349
Gabriel Hall <adrastopoulos@gmail.com>
2025-07-05 16:31:32 -0700

    chore: set up security config

---
+++ src/main/kotlin/org/scottylabs/akita/config/SecurityConfig.kt
@@ -0,0 +1,42 @@
+package org.scottylabs.akita.config
+
+import io.lettuce.core.dynamic.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.formLogin { it.disable()}
+
+        http.authorizeExchange { it.anyExchange().permitAll() }
+
+        http.csrf{ it.disable() }
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.exposedHeaders = listOf("*")
+        configuration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+}
