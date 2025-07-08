649084efaac2d1a695c4f1e301b8f77656755484
Gabriel Hall <adrastopoulos@gmail.com>
2025-07-07 18:27:45 -0700

    fix: add oauth request resolver and related beans

--- src/main/kotlin/org/scottylabs/akita/config/SecurityConfig.kt
+++ src/main/kotlin/org/scottylabs/akita/config/SecurityConfig.kt
@@ -5,9 +5,18 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.RegisterSessionServerAuthenticationSuccessHandler
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler
+import org.springframework.security.web.server.authentication.logout.ServerLogoutHandler
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsConfigurationSource
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
@@ -16,10 +25,30 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 @EnableWebFluxSecurity
 class SecurityConfig(
     @Value("\${app.auth.allowed-origins}") val origins: String,
+    private val clientRegistrationRepository: ReactiveClientRegistrationRepository,
 ) {
     @Bean
     fun authorizedClientRepository(): ServerOAuth2AuthorizedClientRepository {
         return WebSessionServerOAuth2AuthorizedClientRepository()
+    }
+
+//    @Bean
+//    fun authenticationSuccessHandler(): ServerAuthenticationSuccessHandler {
+//    }
+
+    @Bean
+    fun authorizationRequestResolver(): ServerOAuth2AuthorizationRequestResolver {
+        return DefaultServerOAuth2AuthorizationRequestResolver(clientRegistrationRepository)
+    }
+
+    @Bean
+    fun serverLogoutSuccessHandler(): ServerLogoutSuccessHandler {
+        return OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)
+    }
+
+    @Bean
+    fun serverLogoutHandler(): ServerLogoutHandler {
+        return SecurityContextServerLogoutHandler()
     }
 
     @Bean
